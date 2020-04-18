1709e6bacff2af9e3e8d00da987e175310650391
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-04-18 17:51:29 -0300

    paso 8

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -8,9 +8,14 @@ var instancesCount = 0
 func _ready():
 	$RedGenerator.connect("button_down",self,"count_new_instance")
 	$VioletGenerator.connect("button_down",self,"count_new_instance")
-	
+	$Reset.connect("button_down",self,"delete")
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
+	
 func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
